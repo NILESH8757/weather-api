ba05ae61eb168110572aae01a8de18bb9fd8f0e1
Nilesh Kumar <nilesh8757@gmail.com>
2020-10-16 21:15:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,17 +1,16 @@
 # @Date:   2020-10-15T20:09:52+05:30
-# @Last modified time: 2020-10-16T20:25:52+05:30
+# @Last modified time: 2020-10-16T21:13:01+05:30
 # @Developer: nilesh8757
-
 
 
 # import necessary libraries and functions
 from flask import Flask, render_template, jsonify, request
 import requests, json
-import redis_connector as rc
+from redis_connector import Cache
 
 # creating a Flask app
 app = Flask(__name__, template_folder='template')
-c = rc.Cache()
+c = Cache()
 
 def get_data_in_format(scode, response):
 	res = response.json()
@@ -25,13 +24,12 @@ def get_data_in_format(scode, response):
 		"message" : res["properties"]["rawMessage"]
 	}
 	data = json.dumps(data, sort_keys = True, indent = 3)
-	parsed = json.loads(data)
-	return parsed
+	return data
 
 def set_cache(scode, response):
-	parsedData = get_data_in_format(scode, response)
-	c.cache(scode, parsedData)
-	return parsedData
+	data = get_data_in_format(scode, response)
+	c.cache(scode, data)
+	return data
 
 # visit : http://127.0.0.1:5000/
 @app.route('/', methods = ['GET'])
@@ -48,28 +46,32 @@ def home(data):
 
 #get weather info at : http://127.0.0.1:5000/metar/info/<station code>/nocache
 #if nochae == 1, fetch fresh result, else first look for data in cache
+@app.route('/metar/info/<string:scode>', methods = ['GET', 'POST'])
 @app.route('/metar/info/<string:scode>/<int:nocache>', methods = ['GET', 'POST'])
-def disp(scode, nocache):
+def disp(scode, nocache = 0):
     url = "https://api.weather.gov/stations/" + scode + "/observations/latest"
     if nocache == 1:
         response = requests.get(url)
         if(not response.ok):
             return render_template('/error.html')
         else:
-            parsedData = set_cache(scode, response)
+            data = set_cache(scode, response)
+            parsedData = json.loads(data)
             return jsonify({'data': parsedData})
     else:
 	    data_from_cahce = c.get(scode)
 	    if data_from_cahce is None:
-		    response = requests.get(url)
-		    if(not response.ok):
-			    return render_template('/error.html')
-		    else:
-			    parsedData = set_cache(scode, response)
-			    return jsonify({'data': parsedData})
+		 response = requests.get(url)
+		 if(not response.ok):
+                     return render_template('/error.html')
+		 else:
+		     data = set_cache(scode, response)
+		     parsedData = json.loads(data)
+		     return jsonify({'data': parsedData})
 	    else:
-		    parsedData = data_from_cahce.decode('utf-8')
-		    return jsonify({'data': parsedData})
+                 data = data_from_cahce.decode('utf-8')
+                 parsedData = json.loads(data)
+                 return jsonify({'data': parsedData})
 
 
 # driver function
